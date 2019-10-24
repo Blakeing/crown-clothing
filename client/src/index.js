47854ca142cd200a316c80adc4dff4d1736d628b
Blake Ingenthron <blakeingenthron@gmail.com>
2019-10-24 17:22:35 -0500

    updated code to be pwa compliant

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import * as serviceWorker from './serviceWorker';
 
 import { store, persistor } from './redux/store';
 
@@ -20,3 +20,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
--- server.js
+++ server.js
@@ -1,47 +1,52 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const path = require('path');
-const compression = require('compression')
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+const compression = require("compression");
+const enforce = require("express-sslify");
 
-if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression())
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "client/build")));
 
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+	app.get("*", function(req, res) {
+		res.sendFile(path.join(__dirname, "client/build", "index.html"));
+	});
 }
 
 app.listen(port, error => {
-  if (error) throw error;
-  console.log('Server running on port ' + port);
+	if (error) throw error;
+	console.log("Server running on port " + port);
 });
 
-app.post('/payment', (req, res) => {
-  const body = {
-    source: req.body.token.id,
-    amount: req.body.amount,
-    currency: 'usd'
-  };
+app.get("/service-worker.js", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
+});
 
-  stripe.charges.create(body, (stripeErr, stripeRes) => {
-    if (stripeErr) {
-      res.status(500).send({ error: stripeErr });
-    } else {
-      res.status(200).send({ success: stripeRes });
-    }
-  });
+app.post("/payment", (req, res) => {
+	const body = {
+		source: req.body.token.id,
+		amount: req.body.amount,
+		currency: "usd"
+	};
+
+	stripe.charges.create(body, (stripeErr, stripeRes) => {
+		if (stripeErr) {
+			res.status(500).send({ error: stripeErr });
+		} else {
+			res.status(200).send({ success: stripeRes });
+		}
+	});
 });
